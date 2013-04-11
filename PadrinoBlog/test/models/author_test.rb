20ab324ce1a7a26c64782370676d0ff75fa095e8
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-04-10 22:44:05 -0400

    passing 6 assertions with author_test

--- PadrinoBlog/test/models/author_test.rb
+++ PadrinoBlog/test/models/author_test.rb
@@ -1,10 +1,29 @@
+# class Author < ActiveRecord::Base
+# 	has_many :posts
+# 	has_many :comments
+# end
+
+
 require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 
 class AuthorTest < Test::Unit::TestCase
   context "Author Model" do
-    should 'construct new instance' do
-      @author = Author.new
-      assert_not_nil @author
-    end
+  	setup do
+  		@author = Author.new(:name => "John Wilkes Booth")
+  		@author.posts << Post.new(:title => "NYC")
+  		@author.posts << Post.new(:title => "WDI")
+  	end
+
+  	should 'construct new instance' do
+  		assert @author.respond_to?(:posts)
+
+  		assert_equal 2, @author.posts.length
+  		assert @author.posts.is_a?(Array)
+  		assert_equal "WDI", @author.posts.last.title
+  		assert_equal "NYC", @author.posts.first.title
+  		assert_equal "John Wilkes Booth", @author.name
+  	end
+
+
   end
 end
