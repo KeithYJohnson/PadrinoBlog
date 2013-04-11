b75e81f6a02fef3f7ec2c89df0fb8eac4f71e115
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-04-10 22:28:49 -0400

    passing 7 assertions with comment_test

--- PadrinoBlog/test/models/comment_test.rb
+++ PadrinoBlog/test/models/comment_test.rb
@@ -11,21 +11,30 @@ class CommentTest < Test::Unit::TestCase
   context "Comment Model" do
   	setup do
   		@comment = Comment.new
+  		#Tried passing an id with with Author.new and seeing if that would instantiate a post as well with the belongs_to post tests
+  		@comment.author = Author.new(:name => "Keith")
+  		@comment.post = Post.new(:author_id => 1)
+
+
+
   	end
 
 
 
   	should 'belong to post' do
   		assert @comment.respond_to?(:post)
+  		assert @comment.post.is_a? Post
+  		assert_equal @comment.post.author_id, 1
+
   	end
 
   	should 'belong to author' do
   		assert @comment.respond_to?(:author)
-  	end
+  		assert_not_nil @comment.author
+  		assert @comment.author.is_a? Author
 
-    should 'construct new instance' do
-      @comment = Comment.new
-      assert_not_nil @comment
+  		assert_equal "Keith", @comment.author.name
+
     end
   end
 end
